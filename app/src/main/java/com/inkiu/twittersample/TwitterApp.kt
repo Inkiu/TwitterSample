318425cbd07fedd6bed36338faaf84826a378d7b
inkiu <hik0115@gmail.com>
2020-05-18 16:24:31 +0900

    App 단에서 Twitter initailize

--- app/src/main/java/com/inkiu/twittersample/TwitterApp.kt
+++ app/src/main/java/com/inkiu/twittersample/TwitterApp.kt
@@ -1,10 +1,31 @@
 package com.inkiu.twittersample
 
 import com.inkiu.twittersample.di.DaggerAppComponent
+import com.twitter.sdk.android.core.Twitter
+import com.twitter.sdk.android.core.TwitterAuthConfig
+import com.twitter.sdk.android.core.TwitterConfig
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class TwitterApp : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTwitter()
+    }
+
+    private fun initTwitter() {
+        val authConfig = TwitterAuthConfig(
+            BuildConfig.CONSUMER_KEY,
+            BuildConfig.CONSUMER_SECRET
+        )
+
+        val twitterConfig = TwitterConfig.Builder(this)
+            .twitterAuthConfig(authConfig)
+            .build()
+
+        Twitter.initialize(twitterConfig)
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent
