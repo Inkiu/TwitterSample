1c157ec3605114fd30145ff7f46c89a92cc83a05
inkiu <hik0115@gmail.com>
2020-05-18 16:25:14 +0900

    SplashActivity에서 LoginActivity로 가는 로직 추가

--- app/src/main/java/com/inkiu/twittersample/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/inkiu/twittersample/ui/splash/SplashActivity.kt
@@ -1,17 +1,12 @@
 package com.inkiu.twittersample.ui.splash
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.inkiu.twittersample.R
 import com.inkiu.twittersample.ui.base.BaseActivity
 import com.inkiu.twittersample.ui.base.BaseViewModel
-import com.twitter.sdk.android.core.*
-import com.twitter.sdk.android.core.identity.TwitterAuthClient
-import kotlinx.android.synthetic.main.activity_splash.*
+import com.inkiu.twittersample.ui.login.LoginActivity
 import javax.inject.Inject
 
 class SplashActivity : BaseActivity() {
@@ -21,8 +16,6 @@ class SplashActivity : BaseActivity() {
     private val viewModel: SplashViewModel by lazy {
         ViewModelProvider(this, vmFactory)[SplashViewModel::class.java]
     }
-
-    private lateinit var twitterAuthClient: TwitterAuthClient
 
     override fun getViewModel(): BaseViewModel = viewModel
 
@@ -36,54 +29,14 @@ class SplashActivity : BaseActivity() {
          viewModel.viewStateData.observe(this, Observer {
              when (it.loginState) {
                  LoginState.CHECKING -> toast("wait")
-                 LoginState.LOGGED_OUT -> toast("login required")
+                 LoginState.LOGGED_OUT -> navigateLoginActivity()
                  LoginState.LOGGED_IN -> toast("go to home")
              }
          })
     }
 
-    private fun testLogin() {
-        val authConfig = TwitterAuthConfig(
-            "lN87u8bySqvkb4WPB1d21ekpW",
-            "PhCpSmuyLeXxZgeVpbO34SQOrV2MH8fuUlDvV0KwZ5Tvxgr2ir"
-        )
-
-        val twitterConfig = TwitterConfig.Builder(this)
-            .twitterAuthConfig(authConfig)
-            .build()
-
-        Twitter.initialize(twitterConfig)
-
-        twitterAuthClient = TwitterAuthClient()
-
-        twitterSignButton.setOnClickListener {
-            twitterAuthClient.authorize(this, object : Callback<TwitterSession>() {
-                override fun success(result: Result<TwitterSession>?) {
-                    result?.data?.authToken?.let {
-                        viewModel.onTokenArrived(it.token, it.secret)
-                    }
-                }
-
-                override fun failure(exception: TwitterException?) {
-                    toast("트위터 로그인실패")
-                }
-            })
-        }
-
-        viewModel.viewStateData.observe(this, Observer {
-            toast(it.loginState.toString())
-            Log.d("test", "${it.loginState}")
-        })
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        twitterAuthClient.onActivityResult(requestCode, resultCode, data)
-    }
-
-
-    private fun toast(str: String) {
-        Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+    private fun navigateLoginActivity() {
+        startActivity(LoginActivity.newIntent(this))
     }
 
 }
