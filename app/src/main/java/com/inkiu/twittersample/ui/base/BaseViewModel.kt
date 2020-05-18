120076d574bf48cc0405bf4ecac9f5daa7f06b20
inkiu <inkiu0115@estsoft.com>
2020-05-19 08:34:48 +0900

    ViewModel ErrorHandler 추가

--- app/src/main/java/com/inkiu/twittersample/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/inkiu/twittersample/ui/base/BaseViewModel.kt
@@ -2,13 +2,16 @@ package com.inkiu.twittersample.ui.base
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlin.coroutines.CoroutineContext
 
 abstract class BaseViewModel : ViewModel(), CoroutineScope {
 
+    private val errorHandler = CoroutineExceptionHandler { _, throwable -> throwable.printStackTrace() }
+
     override val coroutineContext: CoroutineContext
-        get() = viewModelScope.coroutineContext
+        get() = viewModelScope.coroutineContext + errorHandler
 
     open fun onAttached() {}
 }
