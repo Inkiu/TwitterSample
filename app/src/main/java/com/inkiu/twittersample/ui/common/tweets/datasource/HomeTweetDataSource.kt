bf7a9de5e0d88458c5030562c1240bd0b228654a
inkiu <inkiu0115@estsoft.com>
2020-05-20 23:07:52 +0900

    HomeTweetDataSource 수정

--- app/src/main/java/com/inkiu/twittersample/ui/common/tweets/datasource/HomeTweetDataSource.kt
+++ app/src/main/java/com/inkiu/twittersample/ui/common/tweets/datasource/HomeTweetDataSource.kt
@@ -1,66 +1,66 @@
 package com.inkiu.twittersample.ui.common.tweets.datasource
 
-import androidx.lifecycle.MutableLiveData
-import androidx.paging.ItemKeyedDataSource
-import com.inkiu.data.mapper.Mapper
-import com.inkiu.domain.entities.tweet.TweetEntity
+import com.inkiu.domain.Constant
 import com.inkiu.domain.usecase.GetHomeTweets
-import com.inkiu.twittersample.ui.common.model.Tweet
 import com.inkiu.twittersample.ui.common.model.mapper.TweetEntityTweetMapper
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 class HomeTweetDataSource(
-    private val getHomeTweets: GetHomeTweets,
-    private val scope: CoroutineScope,
+    getHomeTweets: GetHomeTweets,
+    scope: CoroutineScope,
     mapper: TweetEntityTweetMapper
-) : ItemKeyedDataSource<Long, Tweet>(),
-    Mapper<TweetEntity, Tweet> by mapper {
+) : TweetListDataSource<GetHomeTweets.Param>(getHomeTweets, scope, mapper) {
 
-    val state: MutableLiveData<DataSourceState> = MutableLiveData(DataSourceState.Init)
-
-    override fun loadInitial(
-        params: LoadInitialParams<Long>,
-        callback: LoadInitialCallback<Tweet>
-    ) {
-        val initialKey= params.requestedInitialKey ?: 0L
-        scope.launch {
-            state.value = DataSourceState.LoadingInitial
-        }
-        load(initialKey, params.requestedLoadSize, callback)
+    override fun getParam(startId: Long, size: Int): GetHomeTweets.Param {
+        return GetHomeTweets.Param(startId, size)
     }
 
-    override fun loadAfter(params: LoadParams<Long>, callback: LoadCallback<Tweet>) {
-        val key = params.key
-        scope.launch {
-            state.value = DataSourceState.Loading
-        }
-        load(key, params.requestedLoadSize, callback)
-    }
+    override fun getInitialKey(): Long = Constant.INVALID_ID
 
-    override fun loadBefore(params: LoadParams<Long>, callback: LoadCallback<Tweet>) {
-        // no implementation
-    }
-
-    private fun load(startId: Long, size: Int, callback: LoadCallback<Tweet>) {
-        scope.launch {
-            launch {
-                val result = runCatching {
-                    getHomeTweets.execute(
-                        GetHomeTweets.Param(startId, size)
-                    )
-                }
-                if (result.isSuccess) {
-                    state.value = DataSourceState.Success
-                    val tweets = (result.getOrNull() ?: emptyList()).map { map(it) }
-                    callback.onResult(tweets)
-                } else {
-                    state.value = DataSourceState.Failure(result.exceptionOrNull())
-                    callback.onResult(emptyList())
-                }
-            }
-        }
-    }
-
-    override fun getKey(item: Tweet): Long = item.id
+//    val state: MutableLiveData<DataSourceState> = MutableLiveData(DataSourceState.Init)
+//
+//    override fun loadInitial(
+//        params: LoadInitialParams<Long>,
+//        callback: LoadInitialCallback<Tweet>
+//    ) {
+//        val initialKey= params.requestedInitialKey ?: 0L
+//        scope.launch {
+//            state.value = DataSourceState.LoadingInitial
+//        }
+//        load(initialKey, params.requestedLoadSize, callback)
+//    }
+//
+//    override fun loadAfter(params: LoadParams<Long>, callback: LoadCallback<Tweet>) {
+//        val key = params.key
+//        scope.launch {
+//            state.value = DataSourceState.Loading
+//        }
+//        load(key, params.requestedLoadSize, callback)
+//    }
+//
+//    override fun loadBefore(params: LoadParams<Long>, callback: LoadCallback<Tweet>) {
+//        // no implementation
+//    }
+//
+//    private fun load(startId: Long, size: Int, callback: LoadCallback<Tweet>) {
+//        scope.launch {
+//            launch {
+//                val result = runCatching {
+//                    getHomeTweets.execute(
+//                        GetHomeTweets.Param(startId, size)
+//                    )
+//                }
+//                if (result.isSuccess) {
+//                    state.value = DataSourceState.Success
+//                    val tweets = (result.getOrNull() ?: emptyList()).map { map(it) }
+//                    callback.onResult(tweets)
+//                } else {
+//                    state.value = DataSourceState.Failure(result.exceptionOrNull())
+//                    callback.onResult(emptyList())
+//                }
+//            }
+//        }
+//    }
+//
+//    override fun getKey(item: Tweet): Long = item.id
 }
