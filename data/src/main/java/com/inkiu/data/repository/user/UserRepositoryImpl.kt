52677fb48a225b42a5b8277cd6b30e7a892d6806
inkiu <inkiu0115@estsoft.com>
2020-05-20 18:38:58 +0900

    UserRepository 유저 정보 remote로 가져오는 로직 추가

--- data/src/main/java/com/inkiu/data/repository/user/UserRepositoryImpl.kt
+++ data/src/main/java/com/inkiu/data/repository/user/UserRepositoryImpl.kt
@@ -11,20 +11,47 @@ import javax.inject.Singleton
 @Singleton
 class UserRepositoryImpl @Inject constructor(
     private val userDataToEntityMapper: UserDataToEntityMapper,
-    private val localDataSource: UserLocalDataSource
+    private val localDataSource: UserLocalDataSource,
+    private val remoteDataSource: UserRemoteDataSource
 ) : UserRepository {
+
+    private var myProfileId: Long = -1L
 
     override suspend fun getUser(id: Long): UserEntity {
         return getDetailUser(id)
     }
 
     override suspend fun getUsers(id: List<Long>): List<UserEntity> {
-        return localDataSource.getUsers(id)
-            .map { userDataToEntityMapper(it) }
+        return id.map { getDetailUser(it) }
     }
 
     override suspend fun getDetailUser(id: Long): DetailUserEntity {
-        return userDataToEntityMapper(localDataSource.getUser(id))
+        return getUserLocal(id) ?: getUserRemote(id)
     }
 
+    override suspend fun getMyProfile(): DetailUserEntity {
+        return if (myProfileId != -1L) {
+            getDetailUser(myProfileId)
+        } else {
+            remoteDataSource.getMyProfile().also {
+                localDataSource.updateUsers(listOf(it))
+            }.let {
+                userDataToEntityMapper(it)
+            }
+        }
+    }
+
+    private suspend fun getUserLocal(id: Long): DetailUserEntity? {
+        return localDataSource.getUser(id)?.let {
+            userDataToEntityMapper(it)
+        }
+    }
+
+    private suspend fun getUserRemote(id: Long): DetailUserEntity {
+        return remoteDataSource.getUserProfile(id).also {
+            localDataSource.updateUsers(listOf(it))
+        }.let {
+            userDataToEntityMapper(it)
+        }
+    }
 }
