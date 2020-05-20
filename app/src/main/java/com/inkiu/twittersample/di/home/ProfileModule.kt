095981831b73aa33923eb6a4a16cabd2321b5c53
inkiu <inkiu0115@estsoft.com>
2020-05-20 19:45:58 +0900

    ProfileFragment 추가

---
+++ app/src/main/java/com/inkiu/twittersample/di/home/ProfileModule.kt
@@ -0,0 +1,35 @@
+package com.inkiu.twittersample.di.home
+
+import com.inkiu.domain.repositoty.TweetRepository
+import com.inkiu.domain.repositoty.UserRepository
+import com.inkiu.domain.usecase.GetUser
+import com.inkiu.domain.usecase.GetUserTweets
+import com.inkiu.twittersample.common.image.GlideImageLoader
+import com.inkiu.twittersample.common.image.ImageLoader
+import com.inkiu.twittersample.di.PerFragment
+import com.inkiu.twittersample.ui.home.homeprofile.ProfileFragment
+import dagger.Module
+import dagger.Provides
+
+@Module
+class ProfileModule {
+
+    @Provides
+    @PerFragment
+    fun provideGetUserTweets(
+        userRepository: UserRepository,
+        tweetRepository: TweetRepository
+    ) = GetUserTweets(userRepository, tweetRepository)
+
+    @Provides
+    @PerFragment
+    fun provideGetUser(
+        userRepository: UserRepository
+    ) = GetUser(userRepository)
+
+    @Provides
+    @PerFragment
+    fun provideImageLoader(
+        fragment: ProfileFragment
+    ): ImageLoader = GlideImageLoader(fragment)
+}
--- app/src/main/java/com/inkiu/twittersample/ui/home/homeprofile/ProfileFragment.kt
+++ app/src/main/java/com/inkiu/twittersample/ui/home/homeprofile/ProfileFragment.kt
@@ -5,30 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 import com.inkiu.twittersample.R
+import com.inkiu.twittersample.ui.base.BaseFragment
+import com.inkiu.twittersample.ui.home.hometweet.HomeTweetsFragment
+import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ProfileFragment : DaggerFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    companion object {
+        fun newInstance() = ProfileFragment()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -39,19 +31,32 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance() =
-            ProfileFragment().apply {
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        profileRecycler.adapter = Adapter()
     }
+}
+
+class Adapter : RecyclerView.Adapter<Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return LayoutInflater.from(parent.context).inflate(
+            R.layout.item_network_state,
+            parent,
+            false
+        ).let {
+            Holder(it)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        /* no-op */
+    }
+}
+
+class Holder(view: View) : RecyclerView.ViewHolder(view) {
+
 }
