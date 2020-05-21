1cdb6608772cb49ee5241c7ae974938e9d90f3ee
inkiu <hik0115@gmail.com>
2020-05-21 20:56:18 +0900

    ProfileFragment 삭제

--- app/src/main/java/com/inkiu/twittersample/di/HomeModule.kt
+++ app/src/main/java/com/inkiu/twittersample/di/HomeModule.kt
@@ -5,25 +5,16 @@ import com.inkiu.domain.repositoty.TweetRepository
 import com.inkiu.domain.repositoty.UserRepository
 import com.inkiu.domain.usecase.GetHomeTweets
 import com.inkiu.domain.usecase.GetUser
-import com.inkiu.domain.usecase.GetUserDetail
 import com.inkiu.twittersample.common.image.GlideImageLoader
 import com.inkiu.twittersample.common.image.ImageLoader
-import com.inkiu.twittersample.di.PerActivity
-import com.inkiu.twittersample.di.PerFragment
 import com.inkiu.twittersample.ui.home.HomeActivity
-import com.inkiu.twittersample.ui.home.homeprofile.ProfileFragment
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import dagger.android.ContributesAndroidInjector
 import javax.inject.Named
 
 @Module
 abstract class HomeModule {
-
-    @ContributesAndroidInjector(modules = [ProfileModule::class])
-    @PerFragment
-    abstract fun profileFragment(): ProfileFragment
 
     @Module
     companion object {
